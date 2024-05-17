a58550732cf62157183516936721e4b1c91354ce
TR11533D <ciamis666@gmail.com>
2024-05-17 10:58:17 +0700

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def banner():
+	print("""
+\033[96m███████\033[0m╗\033[96m██████\033[0m╗ \033[96m██\033[0m╗  \033[96m██\033[0m╗\033[96m██\033[0m╗  \033[96m██\033[0m╗ \033[96m██████\033[0m╗\033[96m██\033[0m╗  \033[96m██\033[0m╗
+\033[96m██\033[0m╔════╝\033[96m██\033[0m╔══\033[96m██\033[0m╗\033[96m██\033[0m║  \033[96m██\033[0m║\033[96m██\033[0m║  \033[96m██\033[0m║\033[96m██\033[0m╔════╝\033[96m██\033[0m║ \033[96m██\033[0m╔╝
+\033[96m█████\033[0m╗  \033[96m██████\033[0m╔╝\033[96m███████\033[0m║\033[96m███████\033[0m║\033[96m██\033[0m║     \033[96m█████\033[0m╔╝
+\033[96m██\033[0m╔══╝  \033[96m██\033[0m╔══\033[96m██\033[0m╗\033[96m██\033[0m╔══\033[96m██\033[0m║╚════\033[96m██\033[0m║\033[96m██\033[0m║     \033[96m██\033[0m╔═\033[96m██\033[0m╗
+\033[96m██\033[0m║     \033[96m██████\033[0m╔╝\033[96m██\033[0m║  \033[96m██\033[0m║\033[93mv.2024\033[96m \033[96m██\033[0m║╚\033[96m██████\033[0m╗\033[96m██\033[0m║  \033[96m██\033[0m╗
+\033[0m╚═╝     ╚═════╝ ╚═╝  ╚═╝     ╚═╝ ╚═════╝╚═╝  ╚═╝
+\033[0;101m>>> Simple FB Password Cracking Tool | Mr. TR11533D <<<\033[0m""")
+banner()
+
+print("\n\033[94m--- Login Heula Bos Ah ---")
+em = input("\033[93m[*] Email    : \033[0m")
+paswd = input("\033[93m[*] Password : \033[0m")
+sleep(0.5)
+print("\033[92m--- Login sukses! ---\n")
+
+ask = input("\033[93m[+] Target Usr/Eml : \033[0m")
+print("\033[101m=[●>>> Sedang Nge-Crack! <<<●]=\033[0m")
+
+passwords = ["Doraemon Penyeher","Sayang Momok","Indonesia Negeriku","Orang Indo Nih Bos","Bapaklu Hengker","Bocil Jawa","Naruto Selokan","Boboiboy Kentu","Upin Ipin Colmek","Hinata Sagne","Sakura Ngulum","Itachi Nge-Guy","Madara Zombi","Dragon Ball Nanges","Kaguya Picek","Utramen Ngeseng","Dolanan Peli",]
+
+while True:
+	for password in passwords:
+		print("\033[96m●>>> \033[92mTest :\033[0m",password)
+		sleep(1)
